b871010c222b34806e56b7dfacd5a24ff9eba1be
Joaquín Fuenzalida <joaquin.fuenza.v@gmail.com>
2026-08-05 14:18:52 -0400

    fix vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: [],
-  serverActions: {
-    bodySizeLimit: "5mb",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
   },
 };
 
